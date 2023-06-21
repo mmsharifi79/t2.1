2438986510cf70a6b4de7871aa9ddce9037cd0cd
mmsharifi79 <129947258+mmsharifi79@users.noreply.github.com>
2023-06-21 09:25:07 +0330

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ColorChanger from './ColorChanger';
+
+function App() {
+  return (
+    <div className="App">
+      <ColorChanger />
+    </div>
+  );
+}
+
+export default App;
